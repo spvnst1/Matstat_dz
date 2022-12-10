f44e625642c8a3508c17a7e6a77ea8a7abe39448
spvnst1 <82388928+spvnst1@users.noreply.github.com>
2022-12-10 23:29:36 +0300

    Update treug.py

--- treug.py
+++ treug.py
@@ -30,7 +30,7 @@ print(array)
 #sns.kdeplot(array)
 #plt.grid
 
-'''
+
 z=[]
 samples0=np.linspace(start=0,stop=1,num=1000)
 for elem in samples0:
@@ -40,22 +40,10 @@ y_true=[triang.cdf(t,0.4) for t in samples0]
 plt.plot(samples0,z,color='red')
 plt.plot(samples0,y_true,color='black')
 plt.title('Эмпирическая функция распределения при размере выборки 5')
-#plt.stairs(z,color="red")
 
-plt.xlim([0,1]) '''
-#plt.show()
-#print(array)
-'''
-numargs=triang.numargs
-a=0.4
-rv=triang(a)
 
-dist=triang.rvs(0.4,size=1000)
-distripution=np.linspace(0,np.minimum(rv.dist.b,3))
-plt.plot(distripution,rv.pdf(distripution),color="black")
+plt.xlim([0,1]) 
 
-plt.show()
-'''
-'''sns.kdeplot(array)
-plt.grid()
-plt.show() '''
+
+
+plt.show() 
