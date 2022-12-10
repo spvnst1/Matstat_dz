c51a277ad6d472f801cbda360cc336addb3b64ee
spvnst1 <82388928+spvnst1@users.noreply.github.com>
2022-12-10 23:25:10 +0300

    Update piasson.py

--- piasson.py
+++ piasson.py
@@ -41,37 +41,15 @@ Y=[poisson_real(x) for x in X]
 array1=get_array(10000)
 sns.kdeplot(array1)
 plt.grid()
-'''
-#print(array1)
+
 for elem in samples0:
     z.append(imper_func(array1,elem))
 plt.stairs(z,np.append(samples0,1000),color='red')
 plt.xlim([0,30])
 
-#r_values=list(range(100000))
-#dist=[poisson.cdf(r,6) for r in r_values]
-#plt.stairs(dist,color="black")
+r_values=list(range(100000))
+dist=[poisson.cdf(r,6) for r in r_values]
+plt.stairs(dist,color="black")
 plt.stairs(Y,np.append(X,40),color='black')
-plt.title('Эмпирическая функция распределения при размере выборки 1000') '''
+plt.title('Эмпирическая функция распределения при размере выборки 1000') 
 plt.show()
-'''
-list1=[]
-list2=[]
-list3=[]
-list4=[]
-list5=[]
-list6=[]
-list7=[]
-list8=[]
-list0=[5,10,100,200,400,600,800,1000]
-for x in list0:
-    y=[]
-    m=5
-    a=get_array(x)
-    samples = np.linspace(start=0, stop=int(x), num=int(x))
-    for elem in samples:
-        y.append(imper_func(a, elem))
-    list1.append(sqrt(m*x/(m+x))*max(fabs(y-z)))
-    a.clear()
-print(list1)
-'''
